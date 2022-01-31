e36059d0b615d017d0c3e11922f91fed36b43018
john <tierney-john@hotmail.com>
2022-01-31 20:19:38 +0800

    add gateway module

---
+++ gateway/src/main/java/io/github/tierneyjohn/cms/token/TokenConstant.java
@@ -0,0 +1,27 @@
+package io.github.tierneyjohn.cms.token;
+
+/**
+ * <p>
+ *     Token 令牌相关常量
+ * </p>
+ *
+ * @author TierneyJohn@ACGS
+ * <p>
+ * file created on 2022/1/30
+ * </p>
+ */
+public class TokenConstant {
+
+    /** 构造方法私有化 */
+    private TokenConstant() {}
+
+    /** 访问类型 */
+    public static final String ACCESS_TYPE = "access";
+
+    /** 刷新类型 */
+    public static final String REFRESH_TYPE = "refresh";
+
+    /** 令牌应用范围 */
+    public static final String SCOPE = "scope";
+
+}
