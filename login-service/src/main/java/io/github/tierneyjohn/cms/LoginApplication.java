a0605a6f46f2707339cf5a63c8eba7ee3c5d2711
john <tierney-john@hotmail.com>
2022-01-31 20:22:55 +0800

    add login-service module

---
+++ login-service/src/main/java/io/github/tierneyjohn/cms/LoginApplication.java
@@ -0,0 +1,22 @@
+package io.github.tierneyjohn.cms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * <p>
+ *     登录服务启动类
+ * </p>
+ *
+ * @author TierneyJohn@ACGS
+ * <p>
+ * file created on 2022/1/31
+ * </p>
+ */
+@SpringBootApplication
+public class LoginApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LoginApplication.class, args);
+    }
+}
